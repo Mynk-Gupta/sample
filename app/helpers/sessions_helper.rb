5b67ad2e5907142f710c7f543a415181f002a235
MYK <mgmicks@gmail.com>
2013-12-13 18:10:59 +0530

    user update operation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,10 @@ module SessionsHelper
   	
   end
 
+  def current_user?(user)
+  	user== current_user
+  end
+  
   def signed_in?
   	!current_user.nil?
   end
@@ -27,4 +31,13 @@ module SessionsHelper
  	cookies.delete(:remember_token)
  end
 
+ def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
 end
+
